031c7829f6fae85f31cc28f09dbf6ced4435333a
Ahmad Barakat <bkat@Ahmads-MacBook-Air-2.local>
2026-04-05 17:09:40 -0500

    fix

--- FacialRecognition.py
+++ FacialRecognition.py
@@ -1,3 +1,7 @@
+# Facial Recognition and Resizing
+# @Author Ahmad Barakat
+
+# Imports
 import os
 from PIL import Image
 from tqdm import tqdm
@@ -5,24 +9,29 @@ from tqdm import tqdm
 # repo_dir = "ITEC-612-Facial-Recognition"
 # os.chdir(repo_dir)
 
+# Input and output folder
 dataset_dir = "dataset"
 processed_dir = "processed_data"
 os.makedirs(processed_dir, exist_ok=True)
 
+# Resize and color
 target_size = (224,224)
 mode = "RGB"
 
+# Train, val, test
 def preprocess_and_save(input_dir, output_dir):
-    for split in os.listdir(input_dir): # train, val, test
+    for split in os.listdir(input_dir):
         split_input_dir = os.path.join(input_dir, split)
         split_output_dir = os.path.join(output_dir, split)
         os.makedirs(split_output_dir, exist_ok=True)
     
-        for person in os.listdir(split_input_dir):
+        # Label for model to know face to person
+        for person in os.listdir(split_input_dir): 
             person_input_dir = os.path.join(split_input_dir, person)
             person_output_dir = os.path.join(split_output_dir, person)
             os.makedirs(person_output_dir, exist_ok=True)
 
+            #Progress bar
             for img_file in tqdm(os.listdir(person_input_dir), desc=f"Processing {split}/{person}"):
                 img_path = os.path.join(person_input_dir, img_file)
                 try:
@@ -30,10 +39,10 @@ def preprocess_and_save(input_dir, output_dir):
                     img = img.resize(target_size)
                     save_path = os.path.join(person_output_dir, img_file)
                     img.save(save_path)
+                #Failed processing
                 except Exception as e:
                     print(f"Failed to process {img_path}: {e}")
 
-# Step 5 Run Preprocessing
-
+# Run Preprocessing
 preprocess_and_save(dataset_dir, processed_dir)
 print("✅ Preprocessing complete! Check the processed_data folder.")
